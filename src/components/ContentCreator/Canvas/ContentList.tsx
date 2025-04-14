2a54b3d70a9d94318ed981b53ae6e21379d5fcf6
Hampilk <takosadam@outlook.hu>
2025-04-14 22:35:09 +0200

    Update ContentList.tsx

--- src/components/ContentCreator/Canvas/ContentList.tsx
+++ src/components/ContentCreator/Canvas/ContentList.tsx
@@ -3,12 +3,18 @@ import { useDrop } from 'react-dnd';
 import { useLayoutStore } from '@/store/layout';
 import { ContentItem } from './ContentItem';
 
+interface DroppedAsset {
+  id: string;
+  type: string;
+  [key: string]: unknown;
+}
+
 export const ContentList: React.FC = () => {
   const { layout, addItem } = useLayoutStore();
 
-  const [{ isOver }, drop] = useDrop({
+  const [{ isOver }, drop] = useDrop(() => ({
     accept: 'ASSET',
-    drop: (item: any) => {
+    drop: (item: DroppedAsset) => {
       try {
         addItem(item);
       } catch (error) {
@@ -18,7 +24,7 @@ export const ContentList: React.FC = () => {
     collect: (monitor) => ({
       isOver: monitor.isOver(),
     }),
-  });
+  }));
 
   return (
     <div ref={drop} className={`relative ${isOver ? 'bg-blue-100' : ''}`}>
